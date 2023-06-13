2f2787e2fefd189bd960f64a123a380c85d58157
Therese Cecilia Johansson <Theresececilias@gmail.com>
2023-06-13 10:50:04 +0200

    add comments functionality

--- pages/blog/[slug]/partials/add-comment/index.jsx
+++ pages/blog/[slug]/partials/add-comment/index.jsx
@@ -4,23 +4,36 @@ import Input from "@components/input";
 import Label from "@components/label";
 import TextArea from "@components/text-area";
 import styles from "./add-comment.module.css";
+import {
+  addComment,
+  commentsCacheKey,
+} from '../../../../../api-routes/comments';
+import useSWRMutation from 'swr/mutation';
 
 export default function AddComment({ postId }) {
-  const formRef = useRef(); // create a reference
+  const formRef = useRef();
+   const { trigger: addTrigger, isMutating } = useSWRMutation(
+     commentsCacheKey,
+     addComment,
+     {
+       onError: (error) => {
+         console.log(error);
+       },
+     }
+   );
 
-  const handleOnSubmit = (event) => {
+  const handleOnSubmit = async (event) => {
     event.preventDefault();
-    // Alternative way to get the form data
     const formData = new FormData(event.target);
-
     const { author, comment } = Object.fromEntries(formData);
+    const newComment = {
+      author,
+      comment,
+      post_id: postId,
+    };
 
-    /* 
-      Perhaps a good place to add a comment to the database that is associated with the blog post 😙
-      */
-    console.log({ author, comment, postId });
+    const { status, error } = await addTrigger(newComment)
 
-    // Reset the form after submission?
     formRef.current.reset();
   };
 
